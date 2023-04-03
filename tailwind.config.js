6a7500350bb3508333835b5afdff2e9d7bb63b29
Ben Biber <24373331+ben-biber@users.noreply.github.com>
2023-04-03 13:59:02 +0200

    tailwind 1

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["content/**/*.md", "layouts/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#00b6be",
+
+          "secondary": "#006d84",
+
+          "accent": "#f58220",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#007576",
+
+          "success": "#a6ce39",
+
+          "warning": "#fcaf17",
+
+          "error": "#952d4f",
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    require("daisyui"),
+    // ...
+  ],
+};
