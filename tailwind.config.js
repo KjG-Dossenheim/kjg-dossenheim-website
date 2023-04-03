cedad13a279a86cabdad7cf8b147520f1b33b03f
Ben Biber <24373331+ben-biber@users.noreply.github.com>
2023-04-03 23:04:32 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,32 +3,17 @@ module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html"],
   theme: {
     extend: {},
-  },
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-
-          "primary": "#00b6be",
-
-          "secondary": "#006d84",
-
-          "accent": "#f58220",
-
-          "neutral": "#3D4451",
-
-          "base-100": "#FFFFFF",
-
-          "info": "#007576",
-
-          "success": "#a6ce39",
-
-          "warning": "#fcaf17",
-
-          "error": "#952d4f",
-        },
-      },
-    ],
+    colors: {
+      primary: "#00b6be",
+      secondary: "#006d84",
+      accent: "#f58220",
+      neutral: "#3D4451",
+      base: "#FFFFFF",
+      info: "#007576",
+      success: "#a6ce39",
+      warning: "#fcaf17",
+      error: "#952d4f",
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
