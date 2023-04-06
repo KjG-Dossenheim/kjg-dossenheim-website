e67c2b5ac632de266851b38ab56cf4d8f4de03af
Ben Biber <24373331+ben-biber@users.noreply.github.com>
2023-04-06 11:37:19 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["content/**/*.md", "layouts/**/*.html"],
+  content: ["content/**/*.{html,md}", "layouts/**/*.{html,md}"],
   theme: {
     extend: {
       colors: {
-        primary: "#00b6be",
+        primary: {
+          100: "#ccf0f2",
+          200: "#99e2e5",
+          300: "#66d3d8",
+          400: "#33c5cb",
+          500: "#00b6be",
+          600: "#009298",
+          700: "#006d72",
+          800: "#00494c",
+          900: "#002426"
+},
         secondary: "#006d84",
         accent: "#f58220",
         neutral: "#3D4451",
