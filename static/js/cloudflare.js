cc6abb408a55b921aca80182a078cd61cd926eaa
Ben Biber <24373331+ben-biber@users.noreply.github.com>
2023-08-16 14:54:32 +0200

    Update cloudflare.js

--- static/js/cloudflare.js
+++ static/js/cloudflare.js
@@ -1,1 +1,3 @@
-< !--Cloudflare Web Analytics-- ><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "35e3f9fdb1424cf386a29b56efa780c3"}'></script><!--End Cloudflare Web Analytics-- >
+// <!--Cloudflare Web Analytics-->
+<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "35e3f9fdb1424cf386a29b56efa780c3"}'></script>
+// <!--End Cloudflare Web Analytics-- >
